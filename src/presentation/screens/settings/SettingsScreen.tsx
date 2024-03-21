0fa647cbb913390559fe33864cf2884069320890
Furia55 <jsarmiento1985@hotmail.com>
2024-03-21 16:26:56 -0500

    Hata aqui funciona con libreria de iconos para ios y android y sin lib de zustand

---
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native'
+import { styles } from '../../../config'
+
+export const SettingsScreen = () => {
+    return (
+        <View style = { styles.container }>
+            <Text>SettingsScreen</Text>
+        </View>
+    )
+}
